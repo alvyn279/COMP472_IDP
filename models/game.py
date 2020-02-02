9ab5db58534609c583a8ddc9d8da0022f83427e6
Alvyn Duy-Khoi Le <alvyn279@gmail.com>
2020-02-02 17:12:04 -0500

    Refined models initializers

--- models/game.py
+++ models/game.py
@@ -1,4 +1,5 @@
 from constants.constants import MAX_BOARD_SIZE, MIN_BOARD_SIZE
+from strategies.strategies import SearchStrategy
 from string import ascii_uppercase
 
 alphabet = list(ascii_uppercase)
@@ -12,25 +13,25 @@ class Token:
         - black face
     """
 
-    def __init__(self, x, y):
-        self.isWhiteFace = True
-        self.x = x
-        self.y = y
+    def __init__(self, row, col, initial_state):
+        self.x = row
+        self.y = col
+        self.is_white_face = initial_state == 0
         self.__assign_identifier()
 
     def __assign_identifier(self):
         self.identifier = "{}{}".format(alphabet[self.x], self.y)
 
     def flip(self):
-        self.isWhiteFace = not self.isWhiteFace
+        self.is_white_face = not self.is_white_face
 
 
 class Board:
     """
-    Indonesian Dot Puzzle board
+    Indonesian Dot Puzzle board holding 2d of token
     """
 
-    def __init__(self, size=0):
+    def __init__(self, initial_state_stream="000000000", size=MIN_BOARD_SIZE):
 
         if MIN_BOARD_SIZE > size > MAX_BOARD_SIZE:
             print("Given size is out of bounds, defaulting to minimum size...")
@@ -38,4 +39,20 @@ class Board:
         else:
             self.size = size
 
-        self.board = [[Token(i, j) for i in range(size)] for j in range(size)]
+        self.content = [[Token(row, col, initial_state_stream[col + row * self.size])
+                         for col in range(self.size)] for row in range(self.size)]
+
+
+class Solver:
+    """
+    Context for SearchsStrategy/Solver for the puzzle
+    """
+
+    def __init__(self, strategy: SearchStrategy):
+        self.strategy = strategy
+
+    def set_strategy(self, strategy: SearchStrategy):
+        self.strategy = strategy
+
+    def solve(self, initial_board: Board):
+        self.strategy.execute(initial_board)
--- strategies/strategies.py
+++ strategies/strategies.py
@@ -1,6 +1,5 @@
-# algorithms as strategies
-
 from abc import ABC, abstractmethod
+from models.game import Board
 
 
 class SearchStrategy(ABC):
@@ -10,5 +9,18 @@ class SearchStrategy(ABC):
     """
 
     @abstractmethod
-    def execute(self,):
+    def execute(self, initial_board: Board):
+        pass
+
+
+class DepthFirstSearchStrategy(SearchStrategy):
+    """
+    Depth-first search strategy
+    """
+
+    def __init__(self, max_depth):
+        self.max_depth = max_depth
+
+    def execute(self, initial_board: Board):
+        # implement DFS
         pass
